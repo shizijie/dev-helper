c3251181ef78e9ccb934789dae326404337fc537
xuliang <510013803@qq.com>
2019-11-14 11:31:35 +0800

    添加过滤文件

--- buildfile/pfw_t_authority/PfwTAuthorityDao.java
+++
@@ -1,9 +0,0 @@
-package com.shizijie.dev.helper;
-
-/**
- * @author shizijie
- * @version 2019-11-14 11:23:44
- */
-public interface PfwTAuthorityDao {
-
-}
--- buildfile/pfw_t_authority/pfw_t_authority.sql
+++
@@ -1,8 +0,0 @@
--- insert
-insert into pfw_t_authority (ID,name,displayName) values (#{ID},#{name},#{displayName});
-
--- select
-select ID,name,displayName from pfw_t_authority;
-
--- update
-update pfw_t_authority set ID = #{ID},name = #{name},displayName = #{displayName};
