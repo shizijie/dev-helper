d15f926fb04f6cb3cafaebd5983000f86ab58329
xuliang <510013803@qq.com>
2019-11-21 09:57:30 +0800

    add

---
+++ src/main/java/com/shizijie/dev/helper/user/service/UserHelperService.java
@@ -0,0 +1,9 @@
+package com.shizijie.dev.helper.user.service;
+
+/**
+ * @author shizijie
+ * @version 2019-11-20 下午3:37
+ */
+public interface UserHelperService {
+    String getUser(String userType);
+}
