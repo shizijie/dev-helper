c7130ec0c13b2c71d5d0e10999b029d4c40cb8e3
xuliang <510013803@qq.com>
2022-02-12 00:11:19 +0800

    fix

---
+++ dev-helper-web/src/main/java/com/shizijie/dev/helper/web/config/RedisConfig.java
@@ -0,0 +1,44 @@
+package com.shizijie.dev.helper.web.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
+
+/**
+ * @author shizijie
+ * @version 2022-02-11 9:28 PM
+ */
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, Serializable> redisTemplate(LettuceConnectionFactory factory) {
+        RedisTemplate<String, Serializable> template = new RedisTemplate<>();
+        factory.setShareNativeConnection(false);
+        template.setConnectionFactory(factory);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        // key采用String的序列化方式
+        template.setKeySerializer(stringRedisSerializer);
+        // hash的key也采用String的序列化方式
+        template.setHashKeySerializer(stringRedisSerializer);
+        // value序列化方式采用jackson
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        // hash的value序列化方式采用jackson
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
+
+}
--- dev-helper-web/src/main/java/com/shizijie/dev/helper/web/test/TestController.java
+++ dev-helper-web/src/main/java/com/shizijie/dev/helper/web/test/TestController.java
@@ -1,6 +1,11 @@
 package com.shizijie.dev.helper.web.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
 
 /**
  * @author shizijie
@@ -8,4 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @GetMapping("/test")
+    public String test(String name){
+        return (String) redisTemplate.opsForValue().get(name);
+    }
+
+
+    @GetMapping("/test1")
+    public Long test1(String name){
+        long start=System.currentTimeMillis();
+        for(int i=0;i<1000000;i++){
+            CompletableFuture.runAsync(()->{
+                String res=(String) redisTemplate.opsForValue().get(name);
+            });
+        }
+        return System.currentTimeMillis()-start;
+    }
 }
