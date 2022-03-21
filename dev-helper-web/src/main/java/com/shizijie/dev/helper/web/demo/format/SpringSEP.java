4ccd586e1adb562f407ed68c8b8832a6e5bc4bb1
xuliang <510013803@qq.com>
2022-03-21 10:17:18 +0800

    add

---
+++ dev-helper-web/src/main/java/com/shizijie/dev/helper/web/demo/format/SpringSEP.java
@@ -0,0 +1,12 @@
+package com.shizijie.dev.helper.web.demo.format;
+
+/**
+ * @author shizijie
+ * @version 2022-03-20 10:31 PM
+ */
+public class SpringSEP {
+    public static void main(String[] args) {
+        String listFormat="#list1.!['prefix_'+#this.property+#id]";
+
+    }
+}
